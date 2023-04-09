bbd0c80c725c893f0b6f04fbe92802fa57399f91
Aditta Chakraborty <adittacse@gmail.com>
2023-04-09 16:55:39 +0600

    custom message added based on cart products

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import "./cart.css";
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
+    let message;
+    if(cart.length === 0) {
+        message = <p>Please Add Some Products</p>
+    } else {
+        message = <p>Thanks for giving your money</p>
+    }
 
     return (
         <div>
             <h2>Order Summary: {cart.length}</h2>
+            {message}
             {
                 cart.map(tshirt => <p key={tshirt.id}>{tshirt.name}
                     <button onClick={() => handleRemoveFromCart(tshirt.id)}>X</button>
