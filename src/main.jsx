267bcad1b24ef2c0c931602e41dff75981c63016
Aditta Chakraborty <adittacse@gmail.com>
2023-04-10 08:15:59 +0600

    netlify 404 problem solved

--- src/main.jsx
+++ src/main.jsx
@@ -25,6 +25,10 @@ const router = createBrowserRouter([
             {
                 path: "/grandpa",
                 element: <Grandpa></Grandpa>
+            },
+            {
+                path: "*",
+                element: <h2>404 Not Found</h2>
             }
         ]
     }
